0cbbb8ead95cd77854ab0ac16dd768b6c41d887c
Gabriel Branquinho <gabrielranulfo14@gmail.com>
2025-10-28 23:37:23 -0300

    update_mestrado_28_10

--- queries/common_utils.py
+++ queries/common_utils.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+from .monitor_de_memoria import MemoryMonitor
+
 import re
 import sys
 from importlib.metadata import version
 from pathlib import Path
-from subprocess import run
+from subprocess import run, Popen
 from typing import TYPE_CHECKING, Any
 
 from linetimer import CodeTimer
@@ -80,9 +82,28 @@ def execute_all(library_name: str) -> None:
 
     query_numbers = _get_query_numbers(library_name)
 
+    #ajustar o intervalo quando a biblioteca for muito rapida como o caso da polars, pois só assim da para notar a memoria oscilando, em 100 é muito bom mas gera
+    #muita mensagem
+    memoria_monitor = MemoryMonitor(interval_us=1_000_000,log_file="output/run/memory_monitor.csv")
+
     with CodeTimer(name=f"Overall execution of ALL {library_name} queries", unit="s"):
-        for i in query_numbers:
-            run([sys.executable, "-m", f"queries.{library_name}.q{i}"])
+
+        for query_number in query_numbers:
+            # Executa a query e captura o PID do processo
+            process = Popen([sys.executable, "-m", f"queries.{library_name}.q{query_number}"])
+
+            # Configura o PID e detalhes da query no monitor
+            memoria_monitor.set_pid(process.pid)
+            memoria_monitor.set_query_details(query_number=query_number, library_name=library_name)
+
+            # Inicia o monitoramento de memória
+            memoria_monitor.start_monitoring()
+
+            # Aguarda o término do processo
+            process.wait()
+
+            # Para o monitoramento de memória
+            memoria_monitor.stop_monitoring()
 
 
 def _get_query_numbers(library_name: str) -> list[int]:
--- queries/pandas/q1.py
+++ queries/pandas/q1.py
@@ -1,17 +1,12 @@
 from __future__ import annotations
-
 from datetime import date
-
 import pandas as pd
-
 from queries.pandas import utils
 
 Q_NUM = 1
 
-
 def q() -> None:
     line_item_ds = utils.get_line_item_ds
-    # first call one time to cache in case we don't include the IO times
     line_item_ds()
 
     def query() -> pd.DataFrame:
@@ -21,10 +16,6 @@ def q() -> None:
         var1 = date(1998, 9, 2)
 
         filt = line_item_ds[line_item_ds["l_shipdate"] <= var1]
-
-        # This is lenient towards pandas as normally an optimizer should decide
-        # that this could be computed before the groupby aggregation.
-        # Other implementations don't enjoy this benefit.
         filt["disc_price"] = filt.l_extendedprice * (1.0 - filt.l_discount)
         filt["charge"] = (
             filt.l_extendedprice * (1.0 - filt.l_discount) * (1.0 + filt.l_tax)
@@ -44,8 +35,7 @@ def q() -> None:
 
         result_df = agg.sort_values(["l_returnflag", "l_linestatus"])
 
-        return result_df  # type: ignore[no-any-return]
-
+        return result_df  
     utils.run_query(Q_NUM, query)
 
 
