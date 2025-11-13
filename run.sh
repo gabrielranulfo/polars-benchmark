0292cc1ce43c45bdfba32431ff01b33322e9179d
Gabriel Branquinho <gabrielranulfo14@gmail.com>
2025-11-13 20:40:00 -0300

    correcao_modin

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@ export SCALE_FACTOR=1.0
 
 #echo run with cached IO
 #make tables
-make  run-pandas run-polars run-dask run-pyspark
+make  run-modin
+#make  run-pandas run-polars run-dask run-pyspark
 #make  run-polars run-dask run-pandas run-modin run-duckdb run-pyspark
 #make plot
 
