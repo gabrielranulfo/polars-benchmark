15e571a3cb3e3392604ae61d56f18c42d6c7bf2e
Gabriel Branquinho <gabrielranulfo14@gmail.com>
2025-11-13 22:41:04 -0300

    corrigindo make tables

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@ export RUN_LOG_TIMINGS=1
 export SCALE_FACTOR=1.0
 
 #echo run with cached IO
-#make tables
-make  run-modin
-#make  run-pandas run-polars run-dask run-pyspark
+make tables
+#make  run-modin
+make  run-pandas run-polars run-dask run-pyspark
 #make  run-polars run-dask run-pandas run-modin run-duckdb run-pyspark
 #make plot
 
