8b4bcd36a2ec2d9651b1e18095f32732748c4f7b
Gabriel Branquinho <gabrielranulfo14@gmail.com>
2026-01-10 19:04:45 -0300

    correção do arquivo run

--- run.sh
+++ run.sh
@@ -1,24 +1,5 @@
 export RUN_LOG_TIMINGS=1
-#export SCALE_FACTOR=1.0
-
-# Define N_CORES com valor padrão de 2 caso não esteja definido
-export N_CORES=${N_CORES:-8}
-
-export POLARS_MAX_THREADS=$N_CORES
-export SPARK_CORES=$N_CORES
-export DASK_WORKER_CONCURRENCY=$N_CORES
-export MODIN_CPUS=$N_CORES
-
-#export MODIN_MEMORY=8g
-#export MODIN_BLOCK_SIZE=128m
-#export MODIN_MEMORY_PER_NODE=8g
-#export MODIN_MEMORY_PER_NODE=8g
-
-#echo run with cached IO
-
-#export pasta=$PWD
-
-#rm -rfv $pasta/output/*
+#export SCALE_FACTOR=1
 
 make tables SCALE_FACTOR=$SCALE_FACTOR
 
