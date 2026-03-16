d5ada686b0c2250e4e995923c5be075732297ae6
Gabriel Branquinho <gabrielranulfo14@gmail.com>
2026-03-16 20:03:49 -0300

    correção do run
    executar entrada de comandos via terminal ou script como parâmetro

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 export RUN_LOG_TIMINGS=1
 export SCALE_FACTOR=${SCALE_FACTOR:-1}
 
+comando=${comando:-"make run-all"}
+
 #make tables SCALE_FACTOR=$SCALE_FACTOR
 
-make run-all
+eval "$comando"
 
 #make plot
